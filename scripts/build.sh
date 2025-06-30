e2e113e3163f68f71cb180ee77d156bbf1b7c04a
Eoin Carney <eoincarney0@gmail.com>
2025-06-30 22:49:55 +0100

    fix bundling

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 npm install && \
 npx tailwindcss -i ./resources/public/css/styles.css -o ./resources/public/css/tw.css && \
-npx webpack --config webpack.config.js && \
 npx shadow-cljs compile chart && \
+npx webpack --config webpack.config.js && \
 clj -T:build uber
