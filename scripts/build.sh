84e10213b40f67eb7fdb3a626907165ddd16e2b9
Eoin Carney <eoincarney0@gmail.com>
2025-07-04 22:54:35 +0100

    use clojure instead of clj in build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ npm install && \
 npx tailwindcss -i ./resources/public/css/styles.css -o ./resources/public/css/tw.css && \
 npx shadow-cljs compile chart && \
 npx webpack --config webpack.config.js && \
-clj -T:build uber
+clojure -T:build uber
