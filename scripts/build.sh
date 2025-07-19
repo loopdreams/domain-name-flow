5788c3ffbfcfe9581db1d4db6db919bf14fe7856
Eoin Carney <eoincarney0@gmail.com>
2025-07-19 15:57:43 +0100

    set build params for prod

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 npm install && \
 npx tailwindcss -i ./resources/public/css/styles.css -o ./resources/public/css/tw.css && \
-npx shadow-cljs compile chart && \
+npx shadow-cljs release chart && \
 npx webpack --config webpack.config.js && \
 clj -T:build uber
